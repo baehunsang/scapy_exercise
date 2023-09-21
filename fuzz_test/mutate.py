5979d595b9aa7822c0df85502df1f3e38bca2dff
hunsang <hoonsang99@korea.ac.kr>
2023-09-21 18:55:23 +0900

    IMPLIMENT SMALL MUTATION

--- fuzz_test/mutate.py
+++ fuzz_test/mutate.py
@@ -2,21 +2,30 @@ import asyncio
 import json
 import random
 import websockets
+import subprocess
+import threading
+
 
 async def send_fuzzed_boot_notification():
     # WebSocket 연결 설정
     async with websockets.connect("ws://localhost:9000", subprotocols=["ocpp1.6"]) as websocket:
-        for _ in range(10):  
+        for _ in range(100):  
             
             #Mutation ??
             ocpp_message = [
                                 2,
-                                "93e69619-58c7-49a8-bbf6-3f418ef172e9",
+                                "A x 12288",
                                 "BootNotification",
-                                {"chargePointModel":"Optimus",
-                                    "chargePointVendor":"The Mobility House"
+                                {
+                                    "chargePointModel":"Optimus",
+                                    "chargePointVendor":"The Mobility House",
+                                    "chargePointSerialNumber": "111111111111"
                                 }
                             ]
+            # 필드 무작위 변조
+            ocpp_message[3]["chargePointModel"] = random.choice(["ModelA", "0xffffffff", "%99999999999s", "%x%x%x%x%c%n%b", "\xcc\xcc\xcc\xcc%x%x%x%n"])
+            ocpp_message[3]["chargePointVendor"] = random.choice(["VendorX", "VendorY", "VendorZ"])
+            ocpp_message[3]["chargePointSerialNumber"] = random.choice(["\r\n", "%n"])
 
             # 무작위로 변경된 BootNotification 메시지를 서버로 전송
             print("Sending fuzzed message:", ocpp_message)
